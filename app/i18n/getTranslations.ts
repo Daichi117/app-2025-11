8408ba13aca13d860be315faa58173c12bbb7c21
新谷大智 <shintanitaichi@shintanitaichinoMacBook-Air.local>
2025-12-25 21:45:21 +0900

    added backend readmine and requirement

--- app/i18n/getTranslations.ts
+++
@@ -1,6 +0,0 @@
-import { Language } from "./types";
-
-export function getTranslations<T extends Record<string,Record<string,string>>>
-    (translations:Record<Language,T>,lang:Language):T {
-        return translations[lang];
-    }
